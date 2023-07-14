fcd0390c0c4eb346d4b65810a3dd4fc697d1d7e9
Max <maxrrice@hotmail.com>
2023-07-13 20:21:38 -0500

    names list from API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
-console.log("Hello")
+fetch ("https://restcountries.com/v3.1/all?fields=name,flags,languages,capital,cca2")
+.then(function(response) {
+    return response.json()
+})
+.then(function(countryDataArray) {
+    for ( var i = 0; i < countryDataArray.length; i++) {
+        var countryName = countryDataArray[i].name.common
+        console.log(countryName)
+    }
+})
