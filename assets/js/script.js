85ebaa11b2364b92548a6c559e326821b464ea7d
Max <maxrrice@hotmail.com>
2023-07-13 20:35:52 -0500

    more work on pulling country names from API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+var countriesListEl = document.getElementById("countries")
+var dropdownListItems = document.createElement("option")
+var countryArray = []
+
 fetch ("https://restcountries.com/v3.1/all?fields=name,flags,languages,capital,cca2")
 .then(function(response) {
     return response.json()
@@ -5,6 +9,8 @@ fetch ("https://restcountries.com/v3.1/all?fields=name,flags,languages,capital,c
 .then(function(countryDataArray) {
     for ( var i = 0; i < countryDataArray.length; i++) {
         var countryName = countryDataArray[i].name.common
-        console.log(countryName)
+        // console.log(countryName)
+        countryArray[i] = countryName
     }
 })
+console.log(countryArray)
