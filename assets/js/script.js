8352cf4cb5a0b59e911cf53f1a28cf35e6236cad
tjansson-ui <59398067+tjansson-ui@users.noreply.github.com>
2023-07-17 21:05:43 -0500

    countryCCA2 factors into search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var countriesListEl = document.getElementById("countries")
 var userInputEl = document.getElementById("user-selection") 
-
+var countryCode = ""
 var countryNameArray = []
 
 var apiKey = '6ca5adca9d894c52b90506cf4e32af81'
@@ -25,33 +25,10 @@ fetch ("https://restcountries.com/v3.1/independent?status=true&fields=name,langu
     
 })
 
-var buttonClick = document.querySelector('button')
-buttonClick.addEventListener('click', function(event){
-    console.log(countriesListEl.value)
-    event.preventDefault()
-    console.log(userInputEl.value)
-    for (var i =0; i < countryObjectArray.length; i++) {
-        if(countryObjectArray[i].name == userInputEl.value) {
-            return countryObjectArray[i].cca2
-        } 
-    }
-    // var countryIndex = countryObjectArray.findIndex(userInputEl.value)
-    
-    console.log(countryIndex)
-    
-    var countryCode = countryObjectArray[countryIndex].cca2
-    console.log(countryCode)
-})
-
-// }
-// )
-
-
 var proxyUrl = 'https://octoproxymus.herokuapp.com?secret=walrus&url='
 
-function newsCall() {
-    // fetch ('https://newsapi.org/v2/top-headlines?country=us' + '&' + apiKey)
-    fetch (proxyUrl + encodeURIComponent('https://newsapi.org/v2/top-headlines?country=us&apiKey=6ca5adca9d894c52b90506cf4e32af81'))
+function newsCall(countryCode) {
+    fetch (proxyUrl + encodeURIComponent('https://newsapi.org/v2/top-headlines?country='+ countryCode + '&apiKey=' + apiKey))
     .then(function(response) {
     return response.json()
  }) .then(function(value) {
@@ -59,20 +36,24 @@ function newsCall() {
  })
 }
 
-newsCall()
-// 
+var buttonClick = document.querySelector('button')
+buttonClick.addEventListener('click', function(event){
+    event.preventDefault()
+    console.log(userInputEl.value)
+    countryCode = ""
+    for (var i =0; i < countryObjectArray.length; i++) {
+        if(countryObjectArray[i].name.common == userInputEl.value) {
+            console.log(countryObjectArray[i])
+            console.log(countryObjectArray[i].cca2)
+            countryCode = countryObjectArray[i].cca2.toLowerCase()
+        } 
+    }
+    newsCall(countryCode)
 
-// console.log(countryDataArray)
+})
 
-// var codes = countryDataArray.code
 
-// console.log(countryObjectArray)
 
-// countriesListEl.value = 
-// var searchUrl = 'https://restcountries.com/v3.1/independent?name=' + listCountry+ '&fields=cca2'
+// aearataubebgbrcachcncocuczdeegfrgbgrhkhuidieilinitjpkrltlvmamxmyngnlnonzphplptrorsrusasesgsiskthtrtwuausveza
 
-// https://restcountries.com/v3.1/independent?status=true&fields=name,languages,capital,cca2
 
-// 
-// 
-// 
