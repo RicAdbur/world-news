2e3f8129eca004c404ede758d9df8d326ce7c6b2
Max <maxrrice@hotmail.com>
2023-07-18 18:54:48 -0500

    defining variables for selecting HTML elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,15 @@ var countriesListEl = document.getElementById("countries")
 var userInputEl = document.getElementById("user-selection") 
 var countryCode = ""
 var countryNameArray = []
-
 var apiKey = '6ca5adca9d894c52b90506cf4e32af81'
 var countryObjectArray = []
+var capitalEl = document.getElementById("capital-display")
+var languageEl = document.getElementById("language-display")
+var populationEL = document.getElementById("population-display")
+var regionEl = document.getElementById("region-display")
+var flagEL = document.getElementById("country-flag")
+var searchHistoryEl = document.getElementById("search-history")
+var validNewsCountry = ['ae','ar','at','au','be','bg','br','ca','ch','cn','co','cu','cz','de','eg','fr','gb','gr','hk','hu','id','ie','il','in','it','jp','kr','lt','lv','ma','mx','my','ng','nl','no','nz','ph','pl','pt','ro','rs','ru','sa','se','sg','si','sk','th','tr','tw','ua','us','ve','za']
 
 fetch ("https://restcountries.com/v3.1/independent?status=true&fields=name,languages,capital,cca2")
 .then(function(response) {
@@ -51,9 +57,5 @@ buttonClick.addEventListener('click', function(event){
     newsCall(countryCode)
 
 })
-
-
-
-// aearataubebgbrcachcncocuczdeegfrgbgrhkhuidieilinitjpkrltlvmamxmyngnlnonzphplptrorsrusasesgsiskthtrtwuausveza
 
 
