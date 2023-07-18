e09a3916d8f30d6c7a289be2fa76dc20f981c1cb
tjansson-ui <59398067+tjansson-ui@users.noreply.github.com>
2023-07-17 20:31:38 -0500

    connected to news api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 var countriesListEl = document.getElementById("countries")
+var userInputEl = document.getElementById("user-selection") 
 
 var countryNameArray = []
 
-var apiKey = '9cc304d5d23f4ba185e1870cfce21094'
+var apiKey = '6ca5adca9d894c52b90506cf4e32af81'
 var countryObjectArray = []
 
 fetch ("https://restcountries.com/v3.1/independent?status=true&fields=name,languages,capital,cca2")
@@ -26,11 +27,15 @@ fetch ("https://restcountries.com/v3.1/independent?status=true&fields=name,langu
 
 var buttonClick = document.querySelector('button')
 buttonClick.addEventListener('click', function(event){
+    console.log(countriesListEl.value)
     event.preventDefault()
-    console.log(countryObjectArray)
-
-    var countryIndex = countryObjectArray.indexOf(countriesListEl.value)
-    
+    console.log(userInputEl.value)
+    for (var i =0; i < countryObjectArray.length; i++) {
+        if(countryObjectArray[i].name == userInputEl.value) {
+            return countryObjectArray[i].cca2
+        } 
+    }
+    // var countryIndex = countryObjectArray.findIndex(userInputEl.value)
     
     console.log(countryIndex)
     
@@ -41,33 +46,12 @@ buttonClick.addEventListener('click', function(event){
 // }
 // )
 
-// function restfulCountries() {
 
-//     fetch("https://restcountries.com/v3.1/independent?status=true&fields=name,languages,capital,cca2")
-// }
-// var searchCountry = countriesListEl.value
-// fetch( 'https://restcountries.com/v3.1/independent?name=' + searchCountry+ '&fields=cca2')
-
-// console.log(countryDataArray)
-
-// var codes = countryDataArray.code
-
-// console.log(countryObjectArray)
-
-// countriesListEl.value = 
-// var searchUrl = 'https://restcountries.com/v3.1/independent?name=' + listCountry+ '&fields=cca2'
-
-// https://restcountries.com/v3.1/independent?status=true&fields=name,languages,capital,cca2
-
-// fetch ('https://newsapi.org/v2/top-headlines?country=' + '&' apiKey
-// 
-// 
-
-
+var proxyUrl = 'https://octoproxymus.herokuapp.com?secret=walrus&url='
 
 function newsCall() {
     // fetch ('https://newsapi.org/v2/top-headlines?country=us' + '&' + apiKey)
-    fetch ('https://newsapi.org/v2/top-headlines?country=us&apiKey=9cc304d5d23f4ba185e1870cfce21094')
+    fetch (proxyUrl + encodeURIComponent('https://newsapi.org/v2/top-headlines?country=us&apiKey=6ca5adca9d894c52b90506cf4e32af81'))
     .then(function(response) {
     return response.json()
  }) .then(function(value) {
